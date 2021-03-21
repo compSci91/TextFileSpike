b7d33d681119275c85ad2b2ef080d0d2bf8dff06
Joshua Howell <howjosh@gmail.com>
2021-03-21 10:03:09 -0500

    Initial Commit

---
+++ TextFileSpike/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextFileSpike
+//
+//  Created by Joshua Howell on 3/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
